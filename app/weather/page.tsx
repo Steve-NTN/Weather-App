cd8e8cc7e1fbaa9009406e7bbe909cc6a67cd9e4
anhnghia47 <trongnghiafes@gmail.com>
2023-07-23 17:16:30 +0700

    handle share link

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -1,9 +1,19 @@
 import { Weather } from "@/components";
+import Head from "next/head";
 
 type Props = {};
 
 const Page = (props: Props) => {
-  return <Weather />;
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Weather App" />
+        <meta property="og:description" content="Weather App" />
+        <meta property="og:image" content="../assets/imgs/sunset.jpg" />
+      </Head>
+      <Weather />
+    </>
+  );
 };
 
 export default Page;
