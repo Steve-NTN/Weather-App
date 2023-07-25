e945c4651d97d9cfb57eaed9cad2ec64423656b3
anhnghia47 <trongnghiafes@gmail.com>
2023-07-25 09:30:47 +0700

    handle share link p4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import { Inter } from "next/font/google";
 import "@/styles/main.scss";
 import "@/styles/globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "NTN App",
+  description: "NTN App 2023",
+};
 
 export default function RootLayout({
   children,
--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -1,18 +1,29 @@
-import { Weather } from "@/components";
 import Head from "next/head";
+
+import { Weather } from "@/components";
+import { Metadata } from "next";
 
 type Props = {};
 
+export const metadata: Metadata = {
+  title: "Weather App",
+  openGraph: {
+    title: "Weather App",
+    description: "Weather App made by NTN",
+    images: ["../assets/imgs/sunset.jpg"],
+  },
+};
+
 const Page = (props: Props) => {
   return (
-    <>
+    <div>
       <Head>
-        <meta property="og:title" content="Weather App" />
+        <title>Weather App</title>
         <meta property="og:description" content="Weather App made by NTN" />
         <meta property="og:image" content="../assets/imgs/sunset.jpg" />
       </Head>
       <Weather />
-    </>
+    </div>
   );
 };
 
