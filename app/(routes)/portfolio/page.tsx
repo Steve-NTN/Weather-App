158dd0b80346e9b39b69bb9aafb319403db187d1
anhnghia47 <trongnghiafes@gmail.com>
2023-11-12 22:10:54 +0700

    trying to test sharelink

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -6,11 +6,12 @@ type Props = {};
 export const metadata: Metadata = {
   title: "Steve NTN Portfolio",
   icons: {
-    icon: '/icons/portfolio.png',
+    icon: "/icons/portfolio.png",
   },
   openGraph: {
     title: "Steve NTN Portfolio",
     description: "Portfolio made by NTN",
+    images: ["/imgs/sharelink_cv.png"],
   },
 };
 
